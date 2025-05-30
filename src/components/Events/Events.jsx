918df057593b3787396b10257ae252b638c29786
ssudem <sudemjack0983@gmail.com>
2025-05-30 19:37:44 +0530

    22th commit

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -7,33 +7,38 @@ function Events() {
   const [activeTab, setActiveTab] = useState("recent");
 
   return (
-    <section className="event-section container">
-      <h1>EVENTS & WORKSHOPS</h1>
-      <p>
-        Coding is all about practice and dedication towards learning new things.
-        We organise various seminars, workshops, bootcamps, etc to help students
-        to get more exposure into various domains of programming.
-      </p>
+    <section className="event-section">
+      <div className="container event-section-inner">
+        <h1>EVENTS & WORKSHOPS</h1>
+        <p>
+          Coding is all about practice and dedication towards learning new
+          things. We organise various seminars, workshops, bootcamps, etc to
+          help students to get more exposure into various domains of
+          programming.
+        </p>
 
-      <div className="toggle-wrapper">
-        <button
-          className={`toggle-button ${activeTab === "recent" ? "active" : ""} `}
-          onClick={() => setActiveTab("recent")}
-        >
-          Recent Events
-        </button>
-        <button
-          className={`toggle-button ${activeTab === "past" ? "active" : ""}`}
-          onClick={() => setActiveTab("past")}
-        >
-          Past Events
-        </button>
-      </div>
+        <div className="toggle-wrapper">
+          <button
+            className={`toggle-button ${
+              activeTab === "recent" ? "active" : ""
+            } `}
+            onClick={() => setActiveTab("recent")}
+          >
+            Recent Events
+          </button>
+          <button
+            className={`toggle-button ${activeTab === "past" ? "active" : ""}`}
+            onClick={() => setActiveTab("past")}
+          >
+            Past Events
+          </button>
+        </div>
 
-      <div className="event-container">
-        {event[activeTab].map((ev, idx) => (
-          <EventCard key={idx} {...ev} />
-        ))}
+        <div className="event-container">
+          {event[activeTab].map((ev, idx) => (
+            <EventCard key={idx} {...ev} />
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -9,41 +9,43 @@ function Team() {
   const [year, setYear] = useState("2024");
 
   return (
-    <section className="container team-section">
-      <h1>OUR AMAZING TEAM</h1>
-      <p>
-        We are a group of passionate individuals who are dedicated to
-        collaborating and creating amazing things together. Our team is
-        committed to continuous learning and growth, and we are always looking
-        for new ways to improve our skills and knowledge.
-      </p>
-      <div className="dropdown-container">
-        <span> Year : </span>
-        <Select
-          className="dropdown"
-          options={[
-            { value: "2022", label: "2022" },
-            { value: "2024", label: "2024" },
-            // { value: "2025", label: "2025" },
-          ]}
-          values={[{ value: year, label: year }]}
-          placeholder={year}
-          searchable={false}
-          // onChange={(info) => console.log(info[0].value)}
-          onChange={(info) => setYear(info[0].value)}
-        />
-      </div>
-
-      <div className="team-container">
-        {team[year]?.map((member, index) => (
-          <ProfileCard
-            key={member.name + index}
-            image={member.image}
-            name={member.name}
-            title={member.title}
-            socialmedia={member.socialmedia}
+    <section className="team-section">
+      <div className="container team-section-inner">
+        <h1>OUR AMAZING TEAM</h1>
+        <p>
+          We are a group of passionate individuals who are dedicated to
+          collaborating and creating amazing things together. Our team is
+          committed to continuous learning and growth, and we are always looking
+          for new ways to improve our skills and knowledge.
+        </p>
+        <div className="dropdown-container">
+          <span> Year : </span>
+          <Select
+            className="dropdown"
+            options={[
+              { value: "2022", label: "2022" },
+              { value: "2024", label: "2024" },
+              // { value: "2025", label: "2025" },
+            ]}
+            values={[{ value: year, label: year }]}
+            placeholder={year}
+            searchable={false}
+            // onChange={(info) => console.log(info[0].value)}
+            onChange={(info) => setYear(info[0].value)}
           />
-        ))}
+        </div>
+
+        <div className="team-container">
+          {team[year]?.map((member, index) => (
+            <ProfileCard
+              key={member.name + index}
+              image={member.image}
+              name={member.name}
+              title={member.title}
+              socialmedia={member.socialmedia}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
